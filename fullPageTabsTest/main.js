06b5e7fa5fb15e9286a26bf7adfa0e949e9f1258
EbertAndrew <andrewsebertii@gmail.com>
2020-06-09 05:44:13 -0500

    madeFullPageTabs2TryFixNavRequstIsueLoad3dTest

---
+++ fullPageTabsTest/main.js
@@ -0,0 +1,24 @@
+<button class="tablink" onclick="openPage('Grip', this, 'red')" id="defaultOpen">Grip</button>
+<button class="tablink" onclick="openPage('Accessories', this, 'green')">Accessories</button>
+<button class="tablink" onclick="openPage('Merchandise', this, 'blue')">Merchandise</button>
+<button class="tablink" onclick="openPage('Contact', this, 'orange')">Contact</button>
+
+<div id="Grip" class="tabcontent">
+  <h3>Grip</h3>
+  <p>Home</p>
+</div>
+
+<div id="Accessories" class="tabcontent">
+  <h3>Accessories</h3>
+  <p>stuff</p>
+</div>
+
+<div id="Merchandise" class="tabcontent">
+  <h3>Merchandise</h3>
+  <p>stuff2</p>
+</div>
+
+<div id="Contact" class="tabcontent">
+  <h3>Contact</h3>
+  <p>talk</p>
+</div>
